76cb7844b6a7949b6cb3c4a2f8be39be2d1b1451
gabrieldeori <deori.pessoal@gmail.com>
2022-01-07 16:19:20 -0300

    Add path post /user and returning req.body | Add urlencoded extended true and express.json()

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 require('dotenv').config();
-
 const express = require('express');
 
 const app = express();
-
 const PORT = process.env.PORT || 3000;
 
 function listenWithClear() {
@@ -12,6 +10,18 @@ function listenWithClear() {
 }
 
 app.listen(PORT, listenWithClear);
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/ping', (_req, res) => {
+  res.status(200).json({ message: 'Pong' });
+});
+
+app.post('/user', (req, res) => {
+  const { displayName, email, password, image } = req.body;
+  res.status(200).json({ displayName, email, password, image });
+});
 
 // não remova esse endpoint, e para o avaliador funcionar //
 app.get('/', (_request, response) => {
