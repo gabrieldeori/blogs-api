80a121adbc7a3c11a81c7c6bb55ee5b5b59a8deb
gabrieldeori <deori.pessoal@gmail.com>
2022-01-07 15:54:44 -0300

    Add environment variable in PORT of index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+require('dotenv').config();
+
 const express = require('express');
 
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`ouvindo porta ${PORT}`));
 
