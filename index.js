65ef0bcb410c184be7bc08edb3fd5764eaef4661
gabrieldeori <deori.pessoal@gmail.com>
2022-01-07 15:59:35 -0300

    Add listenWithClear function to app.listen()

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log(`ouvindo porta ${PORT}`));
+app.listen(PORT, listenWithClear);
+
+function listenWithClear() {
+  console.clear();
+  console.log(`ouvindo porta ${PORT}`);
+}
 
 // não remova esse endpoint, e para o avaliador funcionar //
 app.get('/', (_request, response) => {
